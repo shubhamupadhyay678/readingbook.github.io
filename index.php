185d538a029def070ff10352df39c305d387d121
Shubham Upadhyay <58855596+shubhamupadhyay678@users.noreply.github.com>
2021-01-08 16:57:49 +0530

    Read More
    ok

---
+++ index.php
@@ -0,0 +1,555 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Media CSS -->
+    <link rel="stylesheet" type="text/css" href="css/media_query.css">
+
+    <!-- fontawesome CSS -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <link rel="icon" href="img/favicon.png" type="image/gif" sizes="16x16">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+
+    <title>Book Reader</title>
+    <style type="text/css">
+    
+
+
+</style>
+  </head>
+  <body>
+     
+     <!-- Back to top button -->
+          <a id="button1"> <i class="fas fa-sort-up" style="font-size: 38px;
+    margin-top: 10px;color: #fff;"></i></a>
+     
+
+<div id="background-header">
+  </div>
+<div class="body-container">
+  <header>
+    <div class="book_icon">
+        <i class="fas fa-book-reader"></i>
+  </div>
+    <button class="my-toggler navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+       <i class="fas fa-grip-lines"></i>
+      <i class="fas fa-grip-lines"></i>
+      </button>
+     <nav class="navbar navbar-expand-lg navbar-light my-nav">
+       <div class="collapse navbar-collapse my-collapse" id="navbarTogglerDemo03">
+      <div class="profile">
+        <img src="img/header.jpg">
+        <a class="navbar-brand " href="index.php"> Book Reader</a>
+       </div>
+      
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 my-ul">
+          <li class="nav-item my-li">
+            <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item my-li">
+            <div class="dropdown">
+               <a class="nav-link" href="#" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Religion</a>
+              <div class="dropdown-menu my-dropdown" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#religion">
+                  <img src="img/drop/img1.png">
+                  <div class="notif-content">
+                    <h3>Hindu</h3>
+                    
+                  </div>
+                </a>
+               <a class="dropdown-item" href="#religion">
+                  <img src="img/drop/img2.jpg">
+                  <div class="notif-content">
+                    <h3>Musluim</h3>
+                    
+                  </div>
+                </a>
+                  <a class="dropdown-item" href="#religion">
+                  <img src="img/drop/img3.png">
+                  <div class="notif-content">
+                    <h3>Sikh</h3>
+                   
+                  </div>
+                </a>
+                <a class="dropdown-item" href="#religion">
+                  <img src="img/drop/img4.jpg">
+                  <div class="notif-content">
+                    <h3>Isai</h3>
+                    
+                  </div>
+                </a>
+              </div>
+            </div>
+          </li>
+          <li class="nav-item my-li">
+            <a class="nav-link" href="#purchase">Purchase</a>
+          </li>
+          <li class="nav-item my-li">
+            <a class="nav-link" href="#contcat">Contcat</a>
+          </li>
+        </ul>
+        <div class="my-search form-inline my-2 my-lg-0">
+          <button class="btn my-2 my-sm-0 search" ><i class="fas fa-search"></i></button>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+<!-------------------- xxxxxxx  xabout us xxx----------->
+<div class="aboutus img-wrapper">
+  
+    <img class="inner-img" src="img/about/img.jpg">
+<div class="card1 card-5">
+    <h3>Mr. Shubham Upadhyay</h3>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+  
+</div>
+
+
+
+</div>
+
+
+<!-------------- counting ------------->
+<section class="counting1">
+  <div class="counr img-wrapper">
+    <img class="inner-img" src="img/about/Book.jpg" width="100%">
+    <div class="centered">
+       <h4>Continue To Read In Life</h4>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      <p><div class="button1">Buy Now</div></p>
+    </div>
+     
+    
+  </div>
+</section>
+
+
+
+<!-- start count stats -->
+
+<section id="counter-stats" class="wow fadeInRight" data-wow-duration="1.4s">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-3 stats">
+        <i class="fa fa-code" aria-hidden="true"></i>
+        <div class="counting" data-count="9000">0</div>
+        <h5>Lines of code</h5>
+      </div>
+
+      <div class="col-md-3 stats">
+        <i class="fa fa-check" aria-hidden="true"></i>
+        <div class="counting" data-count="2800">0</div>
+        <h5>Projects done</h5>
+      </div>
+
+      <div class="col-md-3 stats">
+        <i class="fa fa-smile" aria-hidden="true"></i>
+        <div class="counting" data-count="7500">0</div>
+        <h5>Happy clients</h5>
+      </div>
+
+      <div class="col-md-3 stats">
+        <i class="fa fa-coffee" aria-hidden="true"></i>
+        <div class="counting" data-count="9990">0</div>
+        <h5>Cups of coffee</h5>
+      </div>
+
+
+    </div>
+    <!-- end row -->
+  </div>
+  <!-- end container -->
+
+</section>
+
+<!-- end cont stats -->
+
+
+
+
+<!----------------------- xxxxxxx  Religion xxxxxxx ----------------->
+
+<section id="religion">
+  <div class="container">
+     <h3 id="rel">Our Religion</h3>
+    <div class="reg">
+      <div class="hindu">
+        <h4>Hindu Religion</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card3">
+            <img src="img/reli/img.jpg" alt="Avatar" style="width:100%">
+            <div class="hind">
+              <h4>Ramayan</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1500</p>  
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="card3">
+            <img src="img/reli/img3.jpg" alt="Avatar" style="width:100%">
+            <div class="hind">
+              <h4>Mahabharat</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1200</p>  
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="card3">
+            <img src="img/reli/img5.jpg" alt="Avatar" style="width:100%;height: 325px;">
+            <div class="hind">
+              <h4>Vishnu Puran</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1300</p>  
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="card3">
+            <img src="img/reli/img6.jpg" alt="Avatar" style="width:100%;height: 325px;">
+            <div class="hind">
+              <h4>Shiv Puran</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1400</p>  
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" style="margin-top: 20px; margin-bottom: 20px;">
+        
+        <div class="col-md-4">
+          <div class="hindu">
+            <h4>Muslim Religion</h4>
+            <p>Lorem Ipsum</p>
+          </div>
+          <div class="card3">
+            
+            <img src="img/reli/img2.jpg" alt="Avatar" style="width:100%">
+            <div class="hind">
+              <h4>Qur'an</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1100</p>  
+            </div>
+          </div>
+         
+         </div>
+
+
+
+        <div class="col-md-4">
+          <div class="hindu">
+            <h4>Sikh Religion</h4>
+            <p>Lorem Ipsum</p>
+          </div>
+          <div class="card3">
+           
+            <img src="img/reli/img7.jpg" alt="Avatar" style="width:70%;height: 330px;margin-left: 40px;">
+            <div class="hind">
+              <h4>Guru Granth Sahib</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1600</p>  
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="hindu">
+            <h4>Isai Religion</h4>
+            <p>Lorem Ipsum</p>
+          </div>
+          <div class="card3">
+            <img src="img/reli/img8.jpg" alt="Avatar" style="width:70%;height: 330px;margin-left: 40px;">
+            <div class="hind">
+              <h4>Bible</h4>
+              <b>Buy Now:-</b><a href=""><i class="fas fa-shopping-cart" title="Add to card" style="margin-left: 10px; color: #f80206;"></i></a>
+              <p><b>Price:-</b> Rs. 1500</p>  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!-- Purchase -->
+
+<section id="purchase">
+  <div class="container">
+    <div class="perch">
+        <h3>Purches Whatever You Want</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+      </div>
+    <div class="pur">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card4">
+              <p class="title">PDF</p>
+              <p>Harvard University</p>
+
+              <div class="back">
+              <h3><i class="fa fa-rupee-sign">&nbsp;</i><span>567.06</span></h3>
+              <p><button>Contact</button></p>
+             </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card4">
+              <p class="title">E-BOOK</p>
+              <p>Mumbai University</p>
+
+              <div class="back">
+              <h3><i class="fa fa-rupee-sign"></i>&nbsp;<span>578.05</span></h3>
+              <p><button>Contact</button></p>
+             </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card4">
+              <p class="title">PRINT COPY</p>
+              <p>Harvard University</p>
+
+              <div class="back">
+              <h3><i class="fa fa-rupee-sign"></i>&nbsp;<span>789.09</span></h3>
+              <p><button>Contact</button></p>
+             </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!-- download any way -->
+
+<section id="buy">
+  <div class="container">
+    <div class="by">
+        <h3>Download My App Of Any Plateform</h3>
+        <p>tandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages</p> 
+        <div class="btn"><i class="fa fa-desktop" style="font-size: 25px;"></i>
+        <h5 style="
+    display: initial;">Desktop</h5>
+        
+      </div>
+      <div class="btn"><i class="fa fa-mobile" style="font-size: 25px;"></i><h5 style="
+    display: initial;margin-left: 10px;">Mobile</h5>
+       </div>
+      </div>
+      
+      
+  </div>
+</section>
+
+<!-------------------  client say ---------------->
+
+<section id="our_client">
+  <div class="container">
+     <div class="perch">
+        <h3>What our Client's Say</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card4">
+            <img src="client/img1.jfif" alt="Avatar">
+            <div class="client">
+              <h4><b>John Doe</b></h4> 
+              <p>tandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+              <h5>Go Somewhere</h5> 
+              <p>Architect & Engineer</p> 
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card4">
+            <img src="client/img2.jpeg" alt="Avatar">
+            <div class="client">
+              <h4><b>John Doe</b></h4> 
+              <p>tandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+              <h5>Go Somewhere</h5> 
+              <p>Architect & Engineer</p> 
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card4">
+            <img src="client/img2.jpg" alt="Avatar">
+            <div class="client">
+              <h4><b>John Doe</b></h4> 
+              <p>tandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+              <h5>Go Somewhere</h5> 
+              <p>Architect & Engineer</p> 
+            </div>
+          </div>
+        </div>
+
+      </div>
+  </div>
+</section>
+
+<!----------------   footer ---------- -->
+
+<footer>
+  <div class="container">
+    <div class="foot">
+      <div class="row">
+        <div class="col-md-4">
+          <div id="about">
+          <h4>About</h4>
+            <p>Where does it come from?
+             Contrary to popular belief, Lorem Ipsum is not simply random text</p>
+             <p>Copyright <i class="fas fa-copyright"></i> Design and Developed By <span id="shubham">Shubham Upadhyay (Web Developer)</span></p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div id="about">
+          <h4>Newsletter</h4>
+          <p>Where does it</p>
+          <form class="example" action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search2">
+            <button type="submit"><i class="fa fa-envelope"></i></button>
+          </form>
+        </div>
+      </div>
+        <div class="col-md-4 ">
+          <div id="about">
+          <h4>Follow Me</h4>
+           <p>Let Us Be Social..</p>
+           <div class="social">
+             <ul>
+               <li><i class="fab fa-facebook-f"></i></li>
+               <li><i class="fab fa-instagram"></i></li>
+               <li><i class="fab fa-twitter"></i></li>
+               <li><i class="fab fa-youtube"></i></li>
+               <li><i class="fab fa-linkedin"></i></li>
+             </ul>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+     <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Some content to fill up the page -->
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+    <script type="text/javascript">
+
+      // sscrolll
+        var btn = $('#button1');
+
+        $(window).scroll(function() {
+          if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+          } else {
+            btn.removeClass('show');
+          }
+        });
+
+        btn.on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({scrollTop:0}, '300');
+        });
+
+
+    </script>
+
+
+    <script type="text/javascript">
+
+      // sscrolll
+         $('.btn.search').click(function(){
+            $(".my-search").toggleClass("active")
+          })
+
+          </script>
+
+    <script type="text/javascript">
+      // number count for stats, using jQuery animate
+
+$('.counting').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 3000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+
+});
+    </script>
+
+  </body>
+</html>
+
